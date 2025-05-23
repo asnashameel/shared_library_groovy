5909405be0646b4af30ab1e018a34658289aa777
asnashameel <2020.cse.asna@ekc.edu.in>
2025-05-23 09:34:26 +0000

    added

---
+++ vars/commonbuild.groovy
@@ -0,0 +1,13 @@
+def call() {
+    stage('checkout') {
+         checkout scm
+        }
+    stage('build') {
+         echo "maven building.."
+         sh 'mvn clean install'
+        }
+    stage('Test') {
+         echo "running test"
+         sh 'mvn test'
+        }
+}
